9995446906120969e4eaabea5724746b0c336039
Esteban Muriel <emurielr@eafit.edu.co>
2025-03-02 14:25:02 -0500

    tutorial3-parteB

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,6 @@ Route::get('/contact', function () {
     $phoneNumber = '123456789';
 
     return view('home.contact')->with('title', $data1)->with('email', $email)->with('address', $address)->with('phoneNumber', $phoneNumber);
-
 })->name('home.contact');
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
@@ -33,6 +32,8 @@ Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->nam
 Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
 Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
 Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
-
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
 Auth::routes();
-
